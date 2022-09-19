556b30ad266b85219d961b4c54273a821fce2b5d
Enokek <enok.ekelund@gmail.com>
2022-09-19 12:24:27 +0200

    Array with 5 first prime, trying to link it with input value

---
+++ public/app.js
@@ -0,0 +1,51 @@
+function primenumber () {
+var textbox = document.getElementById("txt").value;
+
+const ArrayList = new Array (2, 3, 5, 7 , 11)
+
+  
+
+
+
+
+console.log(ArrayList);
+
+
+
+}
+
+//textbox -= 1;for (let i = 0; i <  i++);
+
+/*
+if (number === 1) return false
+if (number >= 2) {      // if not equal to 2, so the functon continues
+ for (let i= 2; i < num; i++) {
+  if (number % i === 0) { //check for prime numbers, % gives a rest or not after calculating, useful for checking if a number is even or not
+                        // If there is any remain this function iterates over and over again until the limit is met
+
+  return false;
+   }
+ }
+ return true;
+ */
+
+/* Uppgift A - Primtal
+Beräkna ett visst antal primtal. 
+Skapa ett UI som funkar för följande två scenarion:
+1/Anger man 5, ska ge en lista med de fem första primtalen.
+2/Anger man 5, ska man få se det femte primtalet.
+
+-lös uppgiften först med svaren i console.log
+-bygg gärna vidare på UI, så du kan ta bort console.log
+-kanske en funktion som kollar om ett givet tal är ett primtal eller inte
+-någon loop som letar fram ett visst önskat primtalet (ordningsföljden) i en talserie 1,2,3,4,5,6,7...n
+-glöm inte commits!
+
+*/
+
+
+
+function fibinacci () {
+
+
+}
